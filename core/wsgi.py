117eb26e42af7ff4c73d02f93ab38ea46b5f6773
nill <nillzand@gmail.com>
2025-10-21 12:01:26 +0330

    Initial commit

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,10 +1,20 @@
+# back/core/wsgi.py (MODIFIED)
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+# [NEW] Import WhiteNoise
+from whitenoise import WhiteNoise
+from django.conf import settings
 
-# This line points to your project's settings file.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# core/wsgi.py (Corrected)
+# Get the standard Django application
 application = get_wsgi_application()
+
+# [NEW] Wrap the application with WhiteNoise
+# This will serve files from STATIC_ROOT at the STATIC_URL
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+
+# [NEW] Add your MEDIA_ROOT to be served at the MEDIA_URL
+# This is the key change to make your images work
+application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
